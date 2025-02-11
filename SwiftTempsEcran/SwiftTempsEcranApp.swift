5228903522140e123d926d1406d2c3729a03aea3
Tim M <timothee01meyer@gmail.com>
2025-02-11 08:09:27 +0100

    Initial Commit

---
+++ SwiftTempsEcran/SwiftTempsEcranApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTempsEcranApp.swift
+//  SwiftTempsEcran
+//
+//  Created by meyer timothee on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTempsEcranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
